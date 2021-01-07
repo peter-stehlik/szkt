69b8964a95e66939b629690ee2a4611d84acf6b3
Peter Stehlik <peter@inovative.sk>
2021-01-07 11:06:44 +0100

    add header mobile version

--- web/assets/dev/es6/custom.js
+++ web/assets/dev/es6/custom.js
@@ -14,6 +14,38 @@ let Custom = {
 
 		$links.forEach(el => el.addEventListener("click", toggleNav));
 	},
+	prepareMobSubNav: () => {
+		let $menuLi = document.querySelectorAll(".top-nav > li");
+		let addSubmenu = function(el){
+			let $submenu = el.querySelector("ul");
+
+			if( $submenu ) {
+				// let html = `<a href="javascript:void(0);" class="submenu-toggler js-submenu-toggler"></a>`;
+				let $submenuToggler = document.createElement("a");
+				$submenuToggler.setAttribute("href", "javascript:void(0)");
+				$submenuToggler.setAttribute("class", "submenu-toggler js-submenu-toggler");
+				el.prepend($submenuToggler);
+			}
+		};
+
+		$menuLi.forEach(el => addSubmenu(el));
+
+		Custom.toggleMobSubNav();
+	},
+	toggleMobSubNav: () => {
+		let $links = document.querySelectorAll(".js-submenu-toggler");
+		let toggleSubNav = function(){
+			this.classList.toggle("is-active");
+
+			let $subnav = this.closest("li").querySelector("ul");
+			if ($subnav) {
+				$subnav.classList.toggle("is-active");
+			}
+		}
+
+		$links.forEach(el => el.addEventListener("click", toggleSubNav));
+	},
 }
 
 Custom.toggleMobNav();
+Custom.prepareMobSubNav();
--- web/assets/dist/js/custom.js
+++ web/assets/dist/js/custom.js
@@ -18,6 +18,43 @@ var Custom = {
     $links.forEach(function (el) {
       return el.addEventListener("click", toggleNav);
     });
+  },
+  prepareMobSubNav: function prepareMobSubNav() {
+    var $menuLi = document.querySelectorAll(".top-nav > li");
+
+    var addSubmenu = function addSubmenu(el) {
+      var $submenu = el.querySelector("ul");
+
+      if ($submenu) {
+        // let html = `<a href="javascript:void(0);" class="submenu-toggler js-submenu-toggler"></a>`;
+        var $submenuToggler = document.createElement("a");
+        $submenuToggler.setAttribute("href", "javascript:void(0)");
+        $submenuToggler.setAttribute("class", "submenu-toggler js-submenu-toggler");
+        el.prepend($submenuToggler);
+      }
+    };
+
+    $menuLi.forEach(function (el) {
+      return addSubmenu(el);
+    });
+    Custom.toggleMobSubNav();
+  },
+  toggleMobSubNav: function toggleMobSubNav() {
+    var $links = document.querySelectorAll(".js-submenu-toggler");
+
+    var toggleSubNav = function toggleSubNav() {
+      this.classList.toggle("is-active");
+      var $subnav = this.closest("li").querySelector("ul");
+
+      if ($subnav) {
+        $subnav.classList.toggle("is-active");
+      }
+    };
+
+    $links.forEach(function (el) {
+      return el.addEventListener("click", toggleSubNav);
+    });
   }
 };
 Custom.toggleMobNav();
+Custom.prepareMobSubNav();
