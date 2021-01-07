d27ff05c8c7a0f3216f789e6b320dbd0b14f8713
Peter Stehlik <peter@inovative.sk>
2021-01-07 09:30:47 +0100

    add header desktop version

--- web/assets/dev/es6/custom.js
+++ web/assets/dev/es6/custom.js
@@ -1,3 +1,19 @@
 let Custom = {
-	
+	toggleMobNav: () => {
+		let $links = document.querySelectorAll(".js-nav-toggler");
+		let toggleNav = function () {
+			this.classList.toggle("is-active");
+
+			let $topnav = document.getElementById("topNav");
+			if ($topnav) {
+				$topnav.classList.toggle("is-active");
+			} else {
+				alert("Top Navigation NOT Included!");
+			}
+		};
+
+		$links.forEach(el => el.addEventListener("click", toggleNav));
+	},
 }
+
+Custom.toggleMobNav();
--- web/assets/dist/js/custom.js
+++ web/assets/dist/js/custom.js
@@ -1,3 +1,23 @@
 "use strict";
 
-var Custom = {};
+var Custom = {
+  toggleMobNav: function toggleMobNav() {
+    var $links = document.querySelectorAll(".js-nav-toggler");
+
+    var toggleNav = function toggleNav() {
+      this.classList.toggle("is-active");
+      var $topnav = document.getElementById("topNav");
+
+      if ($topnav) {
+        $topnav.classList.toggle("is-active");
+      } else {
+        alert("Top Navigation NOT Included!");
+      }
+    };
+
+    $links.forEach(function (el) {
+      return el.addEventListener("click", toggleNav);
+    });
+  }
+};
+Custom.toggleMobNav();
